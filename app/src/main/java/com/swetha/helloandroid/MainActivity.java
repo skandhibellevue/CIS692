13ccbaa10fd43abdcd5398b78384197b2bb012cb
Swetha Kandhi <skandhi@my365.bellevue.edu>
2024-12-14 14:06:00 -0600

    hello world changes

---
+++ app/src/main/java/com/swetha/helloandroid/MainActivity.java
@@ -0,0 +1,33 @@
+package com.swetha.helloandroid;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static String MA = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        Log.w(MA, "View resource: " + R.layout.activity_main);
+
+        int count = 0;
+        for(int i = 0; i < 3; i++) {
+            count++;
+        }
+    }
+}
